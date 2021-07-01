201efcddce8d75878c52d88aa6788db873408ed5
Mohamed Gresha <37029038+MohamedGresha@users.noreply.github.com>
2021-07-01 10:08:50 +0200

    Add files via upload

---
+++ Multi-phase Adaptive Competitive Learning Neural Network For Clustering Big Datasets (MACLNN)/6- sixth/run.m
@@ -0,0 +1,100 @@
+base_path = 'E:\1-Paper\PAPER 2\11\2- second_big\2';
+load ('d1.mat');
+
+[X,varmin,varrange]=atscale(X);
+
+% % % st=1;
+% % % en=1500;
+% % % for i=1 :10
+% % %     DD(i).data=X([st:en],:)';
+% % %     st=st+1500;
+% % %     en=en+1500;
+% % %     [bestk,bestw,ACL,clusterid,NN]=atacl_d(DD(i).data');      % Competitive neural network
+% % %     P(i).NN=NN;
+% % %     P(i).bestk=bestk;
+% % %     P(i).ACL=ACL;
+% % %     P(i).clusterid=clusterid;
+% % %     file_name=sprintf('acl_s%d.mat',i);
+% % %     save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','P');
+% % % end 
+%================================================================================================
+% %     i=1;
+% %     DD(i).data=X(:,[5,6,9,8,4])';
+% %     [bestk,bestw,ACL,clusterid,NN]=atacl_d(DD(i).data');      % Competitive neural network
+% %         P(i).NN=NN;
+% %         P(i).bestk=bestk;
+% %         P(i).ACL=ACL;
+% %         P(i).clusterid=clusterid;
+% %         file_name=sprintf('acl_s%d.mat',i);
+% %         save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','P');
+% %     i=2;
+% %     DD(i).data=X(:,[3,10,1,7,2])';
+% %     [bestk,bestw,ACL,clusterid,NN]=atacl_d(DD(i).data');      % Competitive neural network
+% %         P(i).NN=NN;
+% %         P(i).bestk=bestk;
+% %         P(i).ACL=ACL;
+% %         P(i).clusterid=clusterid;
+% %         file_name=sprintf('acl_s%d.mat',i);
+% %         save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','P');
+
+% %================================================================================================
+% st1=1;st2=80001;st3=160001;st4=240001;st5=230001;
+% % en1=1500;
+% for i=1 :10
+%     DD(i).data=X([st1:st1+7999,st2:st2+7999,st3:st3+7999,st4:st4+7999,st5:st5+7999],:)';
+%     st1=st1+8000;
+%     st2=st2+8000;
+%     st3=st3+8000;
+%     st4=st4+8000;
+%     st5=st5+8000;
+%     [bestk,bestw,ACL,clusterid,NN]=atacl_d(DD(i).data');      % Competitive neural network
+%     P(i).NN=NN;
+%     P(i).bestk=bestk;
+%     P(i).ACL=ACL;
+%     P(i).clusterid=clusterid;
+%     file_name=sprintf('acl_s%d.mat',i);
+%     save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','P');
+% end 
+% %================================================================================================
+
+%================================================================================================
+% st1=1;st2=80001;st3=160001;st4=240001;st5=230001;
+st=[1,150001,300001];
+% en1=1500;
+for i=1 :150
+    DD(i).data=X([st(1):st(1)+999,st(2):st(2)+999,st(3):st(3)+999],:)';
+    st(1)=st(1)+1000;
+    st(2)=st(2)+1000;
+    st(3)=st(3)+1000;
+end 
+
+for q=1:10
+    tic
+    a=randi(150,1);
+    [bestk,bestw,ACL,clusterid,NN]=atacl_d(DD(a).data');      % Competitive neural network
+    P6(q).NN=NN;
+    P6(q).bestk=bestk;
+    P6(q).ACL=ACL;
+    P6(q).clusterid=clusterid;
+    P6(q).I=a;
+    time1=toc;
+    P6(q).Time=time1;
+    file_name=sprintf('0_acl_s%d.mat',q);
+    save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','P6'); 
+end
+%================================================================================================
+
+[val6,idx] = max([P6.bestk]);
+for j=1:10
+    tic
+    [bestk,bestw,ACL,clusterid,NMI,NN]=demo_s(val6);     %sequention processing    
+     PP6(j).NN=NN;
+     PP6(j).NMI=NMI;
+     PP6(j).Bestk=bestk;
+     PP6(j).ACL=ACL;
+     PP6(j).Clusterid=clusterid;
+     time2=toc;
+     PP6(j).Time=time2;
+    file_name=sprintf('1_acl_s%d.mat',j);
+    save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid','NN','PP6');
+end
---
+++ Multi-phase Adaptive Competitive Learning Neural Network For Clustering Big Datasets (MACLNN)/6- sixth/runp_s.m
@@ -0,0 +1,68 @@
+% run an algorithm to estimate the number of clusters in a data set
+% for 100 times and save the results
+
+% Ahmed Rafat
+% Mar. 13
+
+% Pima data set
+kopt=[];
+NMI_vec=[];
+bestk_vec=[];
+ACLopt=[];
+
+base_path = 'E:\';
+for q=1:2                %run the Algorithm 10 time to
+    
+    tic
+    
+    [bestk,bestw,ACL,clusterid,NMI,NN]=demo_s;     %sequention processing    
+    
+    time=toc;
+    
+%     kopt=[kopt bestk];
+%     NMI_vec=[NMI_vec;NMI];
+%     bestk_vec=[bestk_vec;bestk];
+%     ACLopt=[ACLopt;ACL];
+    
+    PP(q).NN=NN;
+    PP(q).kopt=bestk;
+    PP(q).NMI=NMI;
+    PP(q).bestk=bestk;
+    PP(q).ACL=ACL;
+    PP(q).time=time;
+    
+    %     NMI_vec=[NMI_vec;NMI];
+    %     bestk_vec=[bestk_vec;bestk];
+    %     ACLopt=[ACLopt;ACL];
+    %subfolder = ('New folder2');
+    file_name=sprintf('SDB_S_%d.mat',q);
+    save(fullfile(base_path,file_name), 'bestk', 'bestw', 'ACL', 'clusterid', 'NMI','time','NN','PP');
+end
+
+save(fullfile(base_path,'SDB_S'), 'NN', 'PP');
+disp('===========================================');
+% disp('kopt=');
+% disp(length(find(kopt==3)));
+[val,id] = min([PP.ACL]);
+%[val,id]=min(ACLopt);
+disp('======> Min ACL <======');
+disp('ACL=');
+disp(val);
+disp('kopt=');
+disp(PP(id).kopt);
+disp('NMIopt=');
+disp(PP(id).NMI);
+disp('time=');
+disp(PP(id).time);
+[val,id]=max([PP.NMI]);
+disp('======> Max NMI<======');
+disp('MAXNMI=');
+disp(val);
+disp('MaxACL=');
+disp(PP(id).ACL);
+disp('kopt=');
+disp(PP(id).kopt);
+disp('time=');
+disp(PP(id).time);
+
+return;
